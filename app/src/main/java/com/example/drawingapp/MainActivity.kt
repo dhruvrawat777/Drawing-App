d12a51ff8a175c4d8625ac5713fc5166aaf6b05a
Dhruv <dhruvrawat2611@gmail.com>
2020-10-08 13:59:54 +0530

    Added share and store

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -5,6 +5,11 @@ import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.media.MediaScannerConnection
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -13,9 +18,13 @@ import android.widget.ImageButton
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.drawToBitmap
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.lang.Exception
 import java.util.jar.Manifest
 
@@ -44,6 +53,17 @@ class MainActivity : AppCompatActivity() {
                 startActivityForResult(pickPhotoIntent,GALLERY)
 
 
+            }
+            else{
+                requestStoragePermission()
+            }
+        }
+        ib_undo.setOnClickListener {
+            drawing_view.onClickUndo() }
+
+        ib_save.setOnClickListener {
+            if(isReadStorageAllowed()){
+                BitmapAsyncTask(getBitmapFromView(fl_drawing_view_container)).execute()
             }
             else{
                 requestStoragePermission()
@@ -139,6 +159,81 @@ class MainActivity : AppCompatActivity() {
         return result==PackageManager.PERMISSION_GRANTED
     }
 
+    private  fun getBitmapFromView(view: View):Bitmap{
+        val returnedBitmap=Bitmap.createBitmap(view.width,view.height,
+            Bitmap.Config.ARGB_8888)
+        val canvas=Canvas(returnedBitmap)
+        val bgDrawable=view.background
+        if(bgDrawable!=null){
+            bgDrawable.draw(canvas)
+        }else{
+            canvas.drawColor(Color.WHITE)
+        }
+
+        view.draw(canvas)
+        return returnedBitmap
+
+    }
+
+    private inner class BitmapAsyncTask(val mBitmap:Bitmap):
+        AsyncTask<Any, Void, String>(){
+        override fun doInBackground(vararg p0: Any?): String {
+            var result=""
+            if(mBitmap!=null){
+                try{
+                    val bytes=ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG,90,bytes)
+                    val f= File(externalCacheDir!!.absoluteFile.toString()+
+                            File.separator+"DrawingApp"+
+                            System.currentTimeMillis()/1000+".png")
+                    val fos=FileOutputStream(f)
+                    fos.write(bytes.toByteArray())
+                    fos.close()
+                    result=f.absolutePath
+
+                }
+                catch(e:Exception){
+                    result=""
+                    e.printStackTrace()
+                }
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if(!result!!.isEmpty()){
+                Toast.makeText(
+                    this@MainActivity,
+                    "File saved :$result",
+                    Toast.LENGTH_SHORT,
+                )
+                    .show()
+            }
+            else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Something went wrong", Toast.LENGTH_SHORT
+                ).show()
+            }
+            MediaScannerConnection.scanFile(
+                this@MainActivity, arrayOf(result),null
+            ){
+                path,uri->val shareIntent=Intent()
+                shareIntent.action=Intent.ACTION_SEND
+                shareIntent.putExtra(Intent.EXTRA_STREAM,uri)
+                shareIntent.type="image/png"
+
+                startActivity(
+                    Intent.createChooser(
+                        shareIntent,"Share"
+                    )
+                )
+            }
+        }
+
+
+    }
     companion object{
         private  const val STORAGE_PERMISSION_CODE=1
         private  const val GALLERY=2
